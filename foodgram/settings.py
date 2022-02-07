81249bb891065fd78604fba3d5a108a710236b42
Radiant Ded <radiantded@gmail.com>
2022-02-07 15:21:10 +0300

    07.02

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -61,9 +61,9 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('SQL_DATABASE'),
-        'USER': os.environ.get('SQL_USER', 'postgres'),
-        'PASSWORD': os.environ.get('SQL_PASSWORD', 'postgres'),
+        'NAME': os.environ.get('SQL_DATABASE', 'foodgram'),
+        'USER': os.environ.get('SQL_USER', 'radiantded'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'deadok'),
         'HOST': os.environ.get('SQL_HOST', 'localhost'),
         'PORT': os.environ.get('SQL_PORT', '5432'),
     }
