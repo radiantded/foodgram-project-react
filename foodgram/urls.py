ead092db03d36f299998e5a07f8128c277244e4e
Radiant Ded <radiantded@gmail.com>
2022-02-12 22:10:10 +0300

    ok

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -12,11 +12,11 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
-    urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
+urlpatterns += staticfiles_urlpatterns()
 
 urlpatterns += [
 
