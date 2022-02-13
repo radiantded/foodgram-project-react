7becb33eeaec647857df5807ca20b5960498f1d5
Radiant Ded <radiantded@gmail.com>
2022-02-14 02:01:08 +0300

    ok

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('password_change/', views.PassChange.as_view(), name='password_change'),
     path('pass_change_done/', views.PassChangeDone.as_view(), name='pass_change_done'),
     path('password_reset/', views.PassReset.as_view(), name='password_reset'),
-    path('pass_reset_done/', views.PassResetDone.as_view(), name='pass_reset_done')
+    path('pass_reset_done/', views.PassResetDone.as_view(), name='pass_reset_done'),
+    path('pass_reset_confirm/<uidb64>/<token>/', views.PassResetConfirm.as_view(), name='pass_reset_confirm'),
+    path('pass_reset_complete/', views.PassResetComplete.as_view(), name='pass_reset_complete')
 ]
